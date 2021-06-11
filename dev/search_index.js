b3d3a82187b4e917952136e4bab220d50ef2dfd3
Documenter.jl <documenter@juliadocs.github.io>
2021-06-11 06:28:20 +0000

    build based on 799ce84

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Photon","category":"page"},{"location":"#Photon","page":"Home","title":"Photon","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Photon.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Photon]","category":"page"}]
+}
