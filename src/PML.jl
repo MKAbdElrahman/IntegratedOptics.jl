89dde30b8ec643cc618d52e5fa29812201f9322e
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2021-06-11 09:29:03 +0200

    update PML docs

--- src/PML.jl
+++ src/PML.jl
@@ -4,7 +4,11 @@ mutable struct PML{ND}
 	_grid::Grid{ND}
 	_s::Array{Complex{Float},ND}	
 end
-	
+
+"""
+	 PML(g::Grid{ND})
+PML constructor that creates an object to be used later as functor.	 
+"""	
 function  PML(g::Grid{ND}) where ND
 			s = ones(Complex{Float},size(g))
 			return PML(g,s)
@@ -13,7 +17,13 @@ _s(d::Real,L::Real;m::Real=3.5,s_min::Real= -1.0) = im*(s_min) * Complex((d/L))^
 
 s(::High,x,d,l)::Complex{Float} = ( _s(x - (l - d)  ,  d))
 s(::Low,x,d,l)::Complex{Float}  = (_s(-x + d , d))
-	
+
+
+
+"""
+	(pml::PML{ND})(dir::Direction,side::Side, ncells = 30)
+apply the pml for a given a direction and side.	
+"""	
 function (pml::PML{ND})(dir::Direction,side::Side, ncells = 30) where ND		
 g = pml._grid 
 I =  CartesianIndices(g,dir,side,ncells) 
@@ -26,7 +36,10 @@ return pml._s
 end
 	
 	
-	
+"""
+	(pml::PML{ND})(dir::Direction, ncells = 30)
+apply the pml for a given a direction for both `HIGH` and `LOW` sides.		
+"""	
 function (pml::PML{ND})(dir::Direction, ncells = 30) where ND
 	pml(dir,LOW,ncells)
 	pml(dir,HIGH,ncells)
