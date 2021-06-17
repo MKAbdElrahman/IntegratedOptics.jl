242135e9af27c1fb4b9c336df2542a7560784c29
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2021-06-17 19:21:04 +0200

    Added Material Type

--- experiments/two_d_example.jl
+++ experiments/two_d_example.jl
@@ -1,69 +1,25 @@
+########################################################
 using Photon
-
-Nx = 20; Ny = 20;
+########################################################
 λ = 1.55; 
-
-dx = λ/Nx;dy = λ/Ny 
-
 Lx = 10 * λ;
 Ly = 10 * λ;
-
-sim = Simulation(λ₀ = λ ;  grid = Grid(extent = (Lx,Ly) , spacing =  (dx,dy) ))
-sim(setsrc!,ẑ,x -> 1 , x-> sqrt((x[1]-Lx/2)^2 + (x[2]-Ly/2)^2) <=  dx)
-
-#sim(setsrc!,PlaneWave(k̂ = (0.0, 2pi/λ), ê = (0,0,1) , a = 1))
-
+sim = Simulation(λ₀ = λ ;  grid = Grid(extent = (Lx,Ly) , spacing =  (λ/20,λ/20) ))
+#########################################################
+sim(setsrc!,PlaneWave(k̂ = (0.0, 2pi/λ), ê = (0,0,1) , a = 1))
 #sim(contourplot, :src_z, real ; xlabel = "x-axis", ylabel = "y-axis", title = "source x")
-
-#sim(setTFSF! ,λ)
+#########################################################
+sim(setTFSF! ,1.5λ)
 #sim(contourplot, :Q, real ; xlabel = "x-axis", ylabel = "y-axis", title = "TFSF")
-
+##########################################################
 sim(setpml!,1λ)
-#sim(contourplot, :S_y, imag ; xlabel = "x-axis", ylabel = "y-axis", title = "PML Y")
-#sim(contourplot, :S_x, imag ; xlabel = "x-axis", ylabel = "y-axis", title = "PML X")
-
-#sim(setϵᵣ!,x̂,x -> 1, x -> sqrt((x[1]-Lx/2)^2 + (x[2]-Ly/2)^2) <=  1.5 )
-#sim(setϵᵣ!,ŷ,x -> 1, x -> sqrt((x[1]-Lx/2)^2 + (x[2]-Ly/2)^2) <=  1.5 )
-#sim(setϵᵣ!,ẑ,x -> 1, x -> sqrt((x[1]-Lx/2)^2 + (x[2]-Ly/2)^2) <=  1.5 )
-
-sim(setϵᵣ!,x̂,x -> 5, x ->  abs(x[1]-Lx/2) <=  3 && abs(x[2]-Ly/2) <=  1.5 )
-sim(setϵᵣ!,ŷ,x -> 5, x ->  abs(x[1]-Lx/2) <=  3 && abs(x[2]-Ly/2) <=  1.5 )
-sim(setϵᵣ!,ẑ,x -> 5, x ->  abs(x[1]-Lx/2) <=  3 && abs(x[2]-Ly/2) <=  1.5 )
-
+##########################################################
+Rectangle(x) =  abs(x[1]-Lx/2) <=  3 && abs(x[2]-Ly/2) <=  1.5
+Si = Material(2,2,2)
+sim(setmaterial!,Si,Rectangle)
 #sim(contourplot,  :ϵᵣ_xx, real ; xlabel = "x-axis", ylabel = "y-axis", title = "ϵ x")
-#sim(contourplot, :ϵᵣ_yy, real ; xlabel = "x-axis", ylabel = "y-axis", title = "ϵ y")
-#sim(contourplot, :ϵᵣ_zz, real ; xlabel = "x-axis", ylabel = "y-axis", title = "ϵ z")
-
-Am = sim(A)
-bv = sim(b)
-
+###########################################################
 x = sim(solve_with_FDFD,using_direct_solver)
 Ex,Ey,Ez = sim(reshapefield,x,Val(3))
-sim(contourplot, Ez, imag ; xlabel = "x-axis", ylabel = "y-axis", title = "Ez")
-
-#=
-using SparseArrays
-O = spzeros(ncells(sim.grid),ncells(sim.grid));
-
-Q = spdiagm(sim.Q[:])
-QM =  [ Q     O       O;
-O      Q     O;
-O      O     Q
-]
-bb = (Am * QM - QM * Am) * bv
-x = Am \ bb
-#(Ex, Ey,Ez) = sim(reshape_vector_field,x)
-#sim(contourplot, Ez, abs ; xlabel = "x-axis", ylabel = "y-axis", title = "Ez")
-#sim(contourplot, Ey, abs ; xlabel = "x-axis", ylabel = "y-axis", title = "Ey")
-#
-
-=#
-
-
-
-
-#dirs  = directions(sim3.grid) 
-#filt =  dirs .!= fill(x̂,3)
-#ex = extent(sim3.grid)[filt] 
-#spa = spacing(sim3.grid)[filt] 
-
+#sim(contourplot, Ez, imag ; xlabel = "x-axis", ylabel = "y-axis", title = "Ez")
+###########################################################
--- src/Interface/Material.jl
+++ src/Interface/Material.jl
@@ -1,10 +1,27 @@
-export setϵᵣ! ,  setμᵣ! 
-
+export setϵᵣ! ,  setμᵣ! , setmaterial! 
+export Material
 struct ϵᵣ end
 struct μᵣ end
 
 const  setϵᵣ! =  ϵᵣ()
 const  setμᵣ! =  μᵣ()
+
+
+struct SetMaterial end
+const setmaterial! = SetMaterial()
+
+struct Material{T1,T2,T3}
+    ϵr_xx::T1
+    ϵr_yy::T2
+    ϵr_zz::T3
+end
+
+function (sim::Simulation)(setmaterial!, mat::Material, region::Function)
+    sim(setϵᵣ!,x̂,x -> mat.ϵr_xx,region)
+    sim(setϵᵣ!,ŷ,x -> mat.ϵr_yy,region)
+    sim(setϵᵣ!,ẑ,x -> mat.ϵr_zz,region)
+end
+
 
 
 function get_ϵ_comp(sim::Simulation{Dim},  dir::Direction{D}) where {D,Dim}
@@ -26,3 +43,4 @@ end
 function (sim::Simulation)(::μᵣ,dir::Direction,val::Function, reg::Function = (x -> true) , gridtype::GridType= p̂) 
 sim(set!, get_μ_comp(sim,  dir),val,reg,gridtype)	
 end		
+
